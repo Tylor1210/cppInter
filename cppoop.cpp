824ebbd6e277d3a1684223bec7c53c25af1d57cb
Tylor C <100950093+Tylor1210@users.noreply.github.com>
2025-11-06 19:47:23 -0500

    Add files via upload

---
+++ cppoop.cpp
@@ -0,0 +1,26 @@
+//Goal: Learn how to declare a reference variable to a Class object.
+
+//Assignment: Assume a class Person has been defined and a new Person object can be created by new Person(). 
+//Write a statement that declares a variable named person while also using it as a reference to a newly created Person object.
+
+
+Person person = new Person();
+
+//Goal: Declare a class type variable and initialize it with a reference to an object.
+
+//Assignment: Assume a class named Menu exists. Write a statement that declares a Menu variable named myMenu and initializes it with 
+//a reference to a newly created Menu object.
+
+
+Menu myMenu = new Menu();
+
+//Goal: Create an object and assign a reference to an existing variable.
+
+//Assignment: Assume also that a reference variable named livingRoomLight of type SmartLight has already been declared with the following statement:
+
+//SmartLight livingRoomLight;
+//Write code that creates a new object of type SmartLight and assigns the reference to livingRoomLight.
+
+
+livingRoomLight = new SmartLight();
+
